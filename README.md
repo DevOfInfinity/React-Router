988cb1de4d1bae88dd6010616adcee7de83abbaf
Joao Nascimento <devnascimento@hotmail.com>
2022-05-26 21:16:52 +0100

    Readme att

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # React Router
 
 Criando Rotas de navegação no React
+
+<img src='./src/assets/ReactRouter.png' alt='PageLearn'>
--- src/views/examples/Home.jsx
+++ src/views/examples/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react"
+import './Home.css'
 
 export default function Home() {
     return (
         <div className="Home">
             <h1>Inicio</h1>
             <h2>Bem Vindo!</h2>
+
+            <p>Matérial de apoio para trabalho e estudo sobre React Router...</p>
         </div>
     )
 }
